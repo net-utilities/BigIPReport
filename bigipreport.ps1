4599819a12f54c32d9ba4307300c4c8f42fb7953
Patrik Jonsson <patrik.jonsson@gmail.com>
2021-04-15 20:33:18 +0200

    Adding support for environment variables (#50)
    * Adding support for environment variables

--- bigipreport.ps1
+++ bigipreport.ps1
@@ -536,14 +536,18 @@ log verbose "Pre-execution checks"
 
 $SaneConfig = $true
 
-if ($null -eq $Global:Bigipreportconfig.Settings.Credentials.Username -or "" -eq $Global:Bigipreportconfig.Settings.Credentials.Username) {
-    log error "No username configured"
-    $SaneConfig = $false
+if ($null -eq $Env:F5_USERNAME) {
+    if ($null -eq $Global:Bigipreportconfig.Settings.Credentials.Username -or "" -eq $Global:Bigipreportconfig.Settings.Credentials.Username) {
+        log error "No username found. You need to either configure the F5 credentials in the configuration file or define an environment variable named F5_USERNAME with the password"
+        $SaneConfig = $false
+    }
 }
 
-if ($null -eq $Global:Bigipreportconfig.Settings.Credentials.Password -or "" -eq $Global:Bigipreportconfig.Settings.Credentials.Password) {
-    log error "No password configured"
-    $SaneConfig = $false
+if ($null -eq $Env:F5_PASSWORD) {
+    if ($null -eq $Global:Bigipreportconfig.Settings.Credentials.Password -or "" -eq $Global:Bigipreportconfig.Settings.Credentials.Password) {
+        log error "No password found. You need to either configure the F5 credentials in the configuration file or define an environment variable named F5_PASSWORD with the password"
+        $SaneConfig = $false
+    }
 }
 
 if ($null -eq $Global:Bigipreportconfig.Settings.DeviceGroups.DeviceGroup -or 0 -eq @($Global:Bigipreportconfig.Settings.DeviceGroups.DeviceGroup.Device).Count) {
@@ -622,9 +626,12 @@ if ($null -eq $Global:Bigipreportconfig.Settings.SupportCheck){
 } else {
     $SupportCheckOption = $Global:Bigipreportconfig.Settings.SupportCheck
     if($SupportCheckOption.Enabled -eq "True") {
-        if ($null -eq $SupportCheckOption.Username -or $SupportCheckOption.Username -eq "" -or $null -eq $SupportCheckOption.Password -or $SupportCheckOption.Password -eq "") {
-            log error "Option Support Check has been enabled but the credentials has not been populated. Either disable the support check or provide credentials"
-            $SaneConfig = $false
+        if ($null -eq $env:F5_SUPPORT_USERNAME -or $null -eq $env:F5_SUPPORT_PASSWORD) {
+            if ($null -eq $SupportCheckOption.Username -or $SupportCheckOption.Username -eq "" -or $null -eq $SupportCheckOption.Password -or $SupportCheckOption.Password -eq "") {
+                log error "Option Support Check has been enabled but the credentials has not been populated."
+                log error "Either disable the support check or provide credentials in the config file or via the environment variables F5_SUPPORT_USERNAME/F5_SUPPORT_PASSWORD"
+                $SaneConfig = $false
+            }
         }
     }
 }
@@ -1204,7 +1211,7 @@ function Get-LTMInformation {
 
     $PoolStatsDict = c@ {}
     If ($MajorVersion -ge 12) {
-        # need 12+ to support members/stats
+        # Need 12+ to support members/stats
         $Response = Invoke-WebRequest -WebSession $Session -SkipCertificateCheck -Uri "https://$LoadBalancerIP/mgmt/tm/ltm/pool/members/stats" |
         ConvertFrom-Json -AsHashtable
         Foreach ($PoolStat in $Response.entries.Values) {
@@ -1217,8 +1224,8 @@ function Get-LTMInformation {
         $ObjTempPool.loadbalancer = $LoadBalancerName
         $ObjTempPool.name = $Pool.fullPath
         if (Get-Member -inputobject $Pool -name 'monitor') {
-            # split into words and take any that start with /
-            # could be at least "<monitor> and <monitor>" or "min 1 of { <monitor> <monitor> }"
+            # Split into words and take any that start with /
+            # Could be at least "<monitor> and <monitor>" or "min 1 of { <monitor> <monitor> }"
             $objTempPool.monitors = [array]$Pool.monitor.split(' ') -match '\/[^ ]*'
         }
         $ObjTempPool.loadbalancingmethod = $Pool.loadBalancingMode
@@ -1258,7 +1265,6 @@ function Get-LTMInformation {
                 } catch {
                     $MemberStats = c@ {}
                 }
-                #.psobject.Properties.Value.nestedStats.entries
             }
             Foreach ($MemberStat in $MemberStats.Values) {
               if ($MemberStat.nestedStats.entries.nodeName.description.contains(':')) {
@@ -1677,8 +1683,18 @@ function GetDeviceInfo {
 
     log verbose "Getting data from $LoadBalancerIP"
 
-    $User = $Global:Bigipreportconfig.Settings.Credentials.Username
-    $Password = $Global:Bigipreportconfig.Settings.Credentials.Password
+    $User = $Env:F5_USERNAME
+    $Password = $Env:F5_PASSWORD
+
+    # If the environment environment variables are not set, use the configuration file instead
+    if ($User -eq "") {
+        $User = $Global:Bigipreportconfig.Settings.Credentials.Username
+    }    
+    if ($Password -eq "") {
+        $Password = $Global:Bigipreportconfig.Settings.Credentials.Password
+    }
+
+    $Password = $Env:F5_PASSWORD
 
     #Create the string that is converted to Base64
     $Credentials = $User + ":" + $Password
@@ -2084,7 +2100,18 @@ if($Global:Bigipreportconfig.Settings.SupportCheck -and $Global:Bigipreportconfi
     }
 
     log info "Support entitlement checks configured, checking support entitlements"
-    $LoginBody = @{"user_id" = $Global:Bigipreportconfig.Settings.SupportCheck.Username; "user_secret" = $Global:Bigipreportconfig.Settings.SupportCheck.Password; "app_id"="support"}
+    $Username = $env:F5_SUPPORT_USERNAME
+    $Password = $env:F5_SUPPORT_PASSWORD
+
+    # If the environment variables are not set, use the configuration file credentials
+    if ($Username -eq "") {
+        $Username = $Global:Bigipreportconfig.Settings.SupportCheck.Username
+    }
+    if ($Password -eq "") {
+        $Password = $Global:Bigipreportconfig.Settings.SupportCheck.Password
+    }
+
+    $LoginBody = @{"user_id" = $Username; "user_secret" = $Password; "app_id"="support"}
 
     Try {
         # Get a session
