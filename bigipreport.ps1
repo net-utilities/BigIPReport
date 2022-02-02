5d3e64f509276be085a45c26bd8b9c934a3c1e97
Tim Riker <Tim@Rikers.org>
2022-02-02 15:21:28 -0700

    5.6.4

--- bigipreport.ps1
+++ bigipreport.ps1
@@ -252,6 +252,7 @@
 #  5.6.2    2021-09-20   cert issuer, udp vs, otherprofiles, csv headings, profiletype in details      Tim Riker       No
 #  5.6.3    2021-09-24   Add function to crawl policies from f5, added some transcript logic for       Marius Bauer    No
 #                        for the readability of the policies.
+#  5.6.4    2021-09-20   track virtualserver protocol. Search as "protocol=udp" to see udp and any     Tim Riker       No
 #
 #  This script generates a report of the LTM configuration on F5 BigIP's.
 #  It started out as pet project to help co-workers know which traffic goes where but grew.
@@ -295,7 +296,7 @@ if ([IO.Directory]::GetCurrentDirectory() -ne $PSScriptRoot) {
 }
 
 #Script version
-$Global:ScriptVersion = "5.6.3"
+$Global:ScriptVersion = "5.6.4"
 
 #Variable used to calculate the time used to generate the report.
 $Global:StartTime = Get-Date
